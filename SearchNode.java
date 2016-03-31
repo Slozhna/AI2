fc5f56e029b04a8fbc1b0494636f3383f05c0c12
eleonoria96@gmail.com <eleonoria96@gmail.com>
2016-03-31 22:29:40 +0300

    Formated and cleaned code

--- SearchNode.java
+++ SearchNode.java
@@ -1,87 +1,75 @@
-public class SearchNode
-{
 
-	private State curState;
-	private SearchNode parent;
-	private double cost; // cost to get to this state
-	private double hCost; // heuristic cost
-	private double fCost; // f(n) cost
+public class SearchNode {
 
-	/**
-	 * Constructor for the root SearchNode
-	 *
-	 * @param s
-	 *            the state passed in
-	 */
-	public SearchNode(State s)
-	{
-		curState = s;
-		parent = null;
-		cost = 0;
-		hCost = 0;
-		fCost = 0;
-	}
+    private State curState;
+    private SearchNode parent;
+    private double cost; // cost to get to this state
+    private double hCost; // heuristic cost
+    private double fCost; // f(n) cost
 
-	/**
-	 * Constructor for all other SearchNodes
-	 *
-	 * @param prev
-	 *            the parent node
-	 * @param s
-	 *            the state
-	 * @param c
-	 *            the g(n) cost to get to this node
-	 * @param h
-	 *            the h(n) cost to get to this node
-	 */
-	public SearchNode(SearchNode prev, State s, double c, double h)
-	{
-		parent = prev;
-		curState = s;
-		cost = c;
-		hCost = h;
-		fCost = cost + hCost;
-	}
+    /**
+     * Constructor for the root SearchNode
+     *
+     * @param s the state passed in
+     */
+    public SearchNode(State s) {
+        curState = s;
+        parent = null;
+        cost = 0;
+        hCost = 0;
+        fCost = 0;
+    }
 
-	/**
-	 * @return the curState
-	 */
-	public State getCurState()
-	{
-		return curState;
-	}
+    /**
+     * Constructor for all other SearchNodes
+     *
+     * @param prev the parent node
+     * @param s the state
+     * @param c the g(n) cost to get to this node
+     * @param h the h(n) cost to get to this node
+     */
+    public SearchNode(SearchNode prev, State s, double c, double h) {
+        parent = prev;
+        curState = s;
+        cost = c;
+        hCost = h;
+        fCost = cost + hCost;
+    }
 
-	/**
-	 * @return the parent
-	 */
-	public SearchNode getParent()
-	{
-		return parent;
-	}
+    /**
+     * @return the curState
+     */
+    public State getCurState() {
+        return curState;
+    }
 
-	/**
-	 * @return the cost
-	 */
-	public double getCost()
-	{
-		return cost;
-	}
+    /**
+     * @return the parent
+     */
+    public SearchNode getParent() {
+        return parent;
+    }
 
-	/**
-	 *
-	 * @return the heuristic cost
-	 */
-	public double getHCost()
-	{
-		return hCost;
-	}
+    /**
+     * @return the cost
+     */
+    public double getCost() {
+        return cost;
+    }
 
-	/**
-	 *
-	 * @return the f(n) cost for A*
-	 */
-	public double getFCost()
-	{
-		return fCost;
-	}
+    /**
+     *
+     * @return the heuristic cost
+     */
+    public double getHCost() {
+        return hCost;
+    }
+
+    /**
+     *
+     * @return the f(n) cost for A*
+     */
+    public double getFCost() {
+        return fCost;
+    }
 }
